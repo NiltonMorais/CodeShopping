2d11a273823fd23e1cef4f4e419e49fc9cd25f78
NiltonMorais <nilton_even@hotmail.com>
2018-10-15 20:15:48 -0300

    Listando categorias de produtos

--- app/Http/Controllers/Api/ProductCategoryController.php
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -2,14 +2,15 @@
 
 namespace CodeShopping\Http\Controllers\Api;
 
+use CodeShopping\Models\Product;
 use Illuminate\Http\Request;
 use CodeShopping\Http\Controllers\Controller;
 
 class ProductCategoryController extends Controller
 {
-    public function index()
+    public function index(Product $product)
     {
-        //
+        return $product->categories;
     }
 
     public function store(Request $request)
