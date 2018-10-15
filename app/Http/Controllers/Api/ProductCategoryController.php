d5de4d74baaed83bfd127f3299e7ee6e24b79102
NiltonMorais <nilton_even@hotmail.com>
2018-10-15 20:05:49 -0300

    Criando endpoints para relacionar produtos e categorias

---
+++ app/Http/Controllers/Api/ProductCategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CodeShopping\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use CodeShopping\Http\Controllers\Controller;
+
+class ProductCategoryController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -22,4 +22,8 @@ class Category extends Model
             ]
         ];
     }
+
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
 }
