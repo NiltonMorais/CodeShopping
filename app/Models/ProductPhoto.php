a684bafe704b1c4b8b9dfa199e53ec183a9ec8bf
NiltonMorais <nilton_even@hotmail.com>
2018-10-16 22:40:25 -0300

    Iniciando modelagem das imagens dos produtos

---
+++ app/Models/ProductPhoto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CodeShopping\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductPhoto extends Model
+{
+    protected $fillable = [
+        'file_name',
+        'product_id'
+    ];
+}
