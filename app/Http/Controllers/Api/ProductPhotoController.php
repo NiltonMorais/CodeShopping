044efed8957132c76678408aff125fb35c099c16
NiltonMorais <nilton_even@hotmail.com>
2018-10-23 13:14:48 -0300

    Resource collection

--- app/Http/Controllers/Api/ProductPhotoController.php
+++ app/Http/Controllers/Api/ProductPhotoController.php
@@ -2,6 +2,7 @@
 
 namespace CodeShopping\Http\Controllers\Api;
 
+use CodeShopping\Http\Resources\ProductPhotoCollection;
 use CodeShopping\Models\Product;
 use CodeShopping\Models\ProductPhoto;
 use Illuminate\Http\Request;
@@ -13,7 +14,7 @@ class ProductPhotoController extends Controller
 
     public function index(Product $product)
     {
-        return ProductPhotoResource::collection($product->photos);
+        return new ProductPhotoCollection($product->photos, $product);
     }
 
     public function store(Request $request)
@@ -26,7 +27,7 @@ class ProductPhotoController extends Controller
         if($photo->product_id != $product->id){
             return response()->json([],404);
         }
-        return $photo;
+        return new ProductPhotoResource($photo);
     }
 
     public function update(Request $request, ProductPhoto $productPhoto)
--- app/Http/Resources/ProductPhotoResource.php
+++ app/Http/Resources/ProductPhotoResource.php
@@ -6,6 +6,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductPhotoResource extends JsonResource
 {
+    private $isCollection;
+
+    public function __construct($resource, $isCollection = false)
+    {
+        parent::__construct($resource);
+        $this->isCollection = $isCollection;
+    }
+
     /**
      * Transform the resource into an array.
      *
@@ -14,12 +22,17 @@ class ProductPhotoResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
+        $data = [
             'id' => $this->id,
             'photo_url' => $this->photo_url,
             'created_at' => $this->created_at->format("d/m/Y - H:i:s"),
-            'updated_at' => $this->updated_at->format("d/m/Y - H:i:s"),
-            'product' => new ProductResource($this->product)
+            'updated_at' => $this->updated_at->format("d/m/Y - H:i:s")
         ];
+
+        if(!$this->isCollection){
+            $data['product'] = new ProductResource($this->product);
+        }
+
+        return $data;
     }
 }
