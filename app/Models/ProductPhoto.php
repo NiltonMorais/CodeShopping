baf88334c1d1dece4626bde213ae2968010594a6
NiltonMorais <nilton_even@hotmail.com>
2018-10-23 12:59:41 -0300

    Entendendo storage privado e publico

--- app/Models/ProductPhoto.php
+++ app/Models/ProductPhoto.php
@@ -37,7 +37,7 @@ class ProductPhoto extends Model
 
     public function getPhotoUrlAttribute(){
         $path = self::photosDir($this->product_id);
-        return asset("algumacoisa/{$path}/{$this->file_name}");
+        return asset("storage/{$path}/{$this->file_name}");
     }
 
     public function product(){
