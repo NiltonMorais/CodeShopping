79bdc4e411abd98e33596551df3a0b1c68160b9f
NiltonMorais <nilton_even@hotmail.com>
2018-10-23 22:56:27 -0300

    Database transaction e tratamento de exceção

--- app/Models/ProductPhoto.php
+++ app/Models/ProductPhoto.php
@@ -24,9 +24,28 @@ class ProductPhoto extends Model
     }
 
     public static function createWithPhotosFiles(int $productId, array $files): Collection{
-        self::uploadFiles($productId, $files);
-        $photos = self::createPhotosModels($productId, $files);
-        return new Collection($photos);
+        try{
+            self::uploadFiles($productId, $files);
+            \DB::beginTransaction();
+            $photos = self::createPhotosModels($productId, $files);
+            \DB::commit();
+            return new Collection($photos);
+        }catch(\Exception $e){
+            \DB::rollback();
+            self::deleteFiles($productId, $files);
+            throw $e;
+        }
+    }
+
+    private static function deleteFiles(int $productId, array $files){
+        /** @var UploadedFile $file */
+        foreach ($files as $file){
+            $path = self::photosPath($productId);
+            $photoPath = "{$path}/{$$file->hashName()}";
+            if(file_exists($photoPath)){
+                \File::delete($photoPath);
+            }
+        }
     }
 
     public static function uploadFiles(int $productId, array $files){
