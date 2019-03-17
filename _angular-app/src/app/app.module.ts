9985054429c2f4657fab38f17ff083d973d75413
Nilton Morais <niltonmorais.code@gmail.com>
2019-03-17 11:53:13 -0300

    definindo página padrão

--- _angular-app/src/app/app.module.ts
+++ _angular-app/src/app/app.module.ts
@@ -14,6 +14,11 @@ const routes: Routes = [
     },
     {
         path: 'categories/list', component: CategoryListComponent
+    },
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
     }
 ];
 
