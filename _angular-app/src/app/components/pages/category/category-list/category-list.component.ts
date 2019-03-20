0b12756df19ad556842caa62ad2e0cb1a94f2c31
Nilton Morais <niltonmorais.code@gmail.com>
2019-03-19 22:54:47 -0300

    criando cadastro de categorias

--- _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
+declare let $;
+
 @Component({
     selector: 'app-category-list',
     templateUrl: './category-list.component.html',
@@ -8,7 +10,11 @@ import {HttpClient} from "@angular/common/http";
 })
 export class CategoryListComponent implements OnInit {
 
-    categories: Array<{id: number,name: string, active: boolean, created_at: {date: string}}> = [];
+    categories: Array<{ id: number, name: string, active: boolean, created_at: { date: string } }> = [];
+
+    category = {
+        name: ''
+    };
 
     constructor(private http: HttpClient) {
     }
@@ -19,11 +25,24 @@ export class CategoryListComponent implements OnInit {
 
     getCategories() {
         const token = window.localStorage.getItem('token');
-        this.http.get<{data: Array<{id: number,name: string, active: boolean, created_at: {date: string}}>}>('http://localhost:8000/api/categories', {
+        this.http.get<{ data: Array<{ id: number, name: string, active: boolean, created_at: { date: string } }> }>('http://localhost:8000/api/categories', {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
         })
             .subscribe(response => this.categories = response.data);
+    }
+
+    submit() {
+        const token = window.localStorage.getItem('token');
+        this.http.post('http://localhost:8000/api/categories', this.category, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+            .subscribe((category) => {
+                $('#exampleModal').modal('hide');
+                this.getCategories();
+            });
     }
 }
